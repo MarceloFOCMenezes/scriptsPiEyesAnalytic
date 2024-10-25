c2eaab2cb28ad8da557425f84bd109c915ec0b2a
Luiz Gustavo Dantas <160529066+Tetinho-Dantas@users.noreply.github.com>
2024-10-25 15:19:23 -0300

    Set Interval Ajustado

--- capturaPI.py
+++ capturaPI.py
@@ -60,7 +60,7 @@ def inserirDados(dado, fkMaquina, fkRecurso, bd):
         cursor.close()
         
 
-def monitor_system(bd, idMaquina, interval=5000):
+def monitor_system(bd, idMaquina, interval=10000):
     while True:
         bytesRecebidos, bytesEnviados = receberRede()
         cpu = receberCpu()
